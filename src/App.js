d37183f1e5c375cddcc23971cb9498265958ae29
PratyushPoddar07 <shuvampoddar018@gmail.com>
2024-09-23 03:52:33 +0530

    TextForm-Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 const name ="Pratyush Poddar";
 function App() {
@@ -10,7 +11,11 @@ function App() {
 {/* <Navbar   />  */}
 
 <Navbar  title="CloudSentinel" aboutText="About Cloud" /> 
+<div className="container my-3">
 
+<TextForm heading="Enter the text to analyze below"/>
+
+</div>
     </>
   );
 } 
